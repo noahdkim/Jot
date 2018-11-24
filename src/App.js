47fd98ee8762821995a10c0dc29d9d888db985f4
Noah Kim <nk8na@virginia.edu>
2018-11-24 10:12:32 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Calendar as Cal} from 'react-calendar';
+import 'typeface-roboto';
 
-class App extends Component {
+
+
+class Note extends Component {
+  constructor(props){
+    super(props);
+    this.onChange = date => this.setState({ date })
+  }
+  render(){
+    return(
+      <div>
+        {this.props.date}
+      </div>
+    );
+  }
+
+}
+
+class Calendar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      date: props.date,
+    }
+    this.calendarType = props.calendarType
+    this.onChange = this.props.updateDate
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <div className="calendar">
+          <Cal
+            calendarType={this.calendarType}
+            onChange={this.onChange}
+            value={this.state.date}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  displayDate(){
+    alert(this.date);
+  }
+
+  updateDate(newDate){
+    this.setState({
+      date: newDate
+    });
+  }
+
+
+  state = {
+    date: new Date(),
+  }
+
+
+  render() {
+    console.log(this.state.date)
+    return (
+      <div>
+        <div className="calendar">
+          <Calendar
+          calendarType = "US"
+          date = {this.state.date}
+          changeDate={this.updateDate}
+          />
+        </div>
+
+        <div className="note">
+          <Note displayDate={this.displayDate}/>
+        </div>
       </div>
     );
   }
